f54ddfcbbc667ca8c1c7bd126c505c74394dfba9
Kevin Bollman <digitalkdogg@gmail.com>
2023-03-24 20:14:32 -0500

    add test ajax call to componentdidupdate

--- src/App.js
+++ src/App.js
@@ -12,14 +12,19 @@ class App extends React.Component {
         this.state = {
           error: null,
           isLoaded: false,
-          items: []
+          items: [],
+          bearer: '',
+          //baseurl: 'https://api.tweetlizer.com/public'
+          baseurl : 'http://localhost:8000' //need to move to env variable
         };
       }
     
       componentDidMount() {
-         let baseurl ='https://api.tweetlizer.com/public'
+        // let baseurl ='https://api.tweetlizer.com/public'
          if (process.env.NODE_ENV==='development') {
-             baseurl = 'http://localhost:8000'
+            this.setState({
+              baseurl : 'http://localhost:8000'
+            })
         }
 
         var key = process.env.REACT_APP_NOT_SECRET_CODE.split('=');
@@ -32,14 +37,15 @@ class App extends React.Component {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ key: key })
       };
-        fetch(baseurl + "/api/auth", requestOptions)
+        fetch(this.state.baseurl + "/api/auth", requestOptions)
           .then(res => res.json())
           .then(
             (result) => {
-                console.log(result)
+                console.log(result.bearer);
               this.setState({
                 isLoaded: true,
-                items: result.items
+                //items: result.items
+                bearer: result.bearer
               });
             },
             // Note: it's important to handle errors here
@@ -48,13 +54,73 @@ class App extends React.Component {
             (error) => {
               this.setState({
                 isLoaded: true,
-                error
+                //error
               });
             }
           )
+
+          
+      }
+
+      componentDidUpdate() {
+        if (this.state.isLoaded == true) {
+          const requestOptions = {
+            method: 'GET',
+           // headers: { 'Content-Type': 'application/json' },
+          //  body: JSON.stringify({ key: key })
+        };
+          fetch(this.state.baseurl + "/api/test/"+this.state.bearer, requestOptions)
+            .then(res => res.json())
+            .then(
+              (result) => {
+                console.log(result);
+              },
+              // Note: it's important to handle errors here
+              // instead of a catch() block so that we don't swallow
+              // exceptions from actual bugs in components.
+              (error) => {
+                this.setState({
+                  isLoaded: true,
+                  //error
+                });
+              })
+        }
       }
 
     render() {
+     // var _this = this;
+//      setTimeout(function () {//
+//        let baseurl ='https://api.tweetlizer.com/public'
+//        if (process.env.NODE_ENV==='development') {
+//            baseurl = 'http://localhost:8000'
+//       }
+//        const requestOptions = {
+//          method: 'GET',
+//          //headers: { 'Content-Type': 'application/json' },
+//          //body: JSON.stringify({ key: key })
+//      };
+//        fetch(baseurl + "/api/test/" + _this.state.bearer, requestOptions)
+//          .then(res => res.json())
+//          .then(
+//            (result) => {
+//                console.log(result);
+              // this.setState({
+              //  isLoaded: true,
+                //items: result.items
+               // bearer: result.bearer
+             // });
+//            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+//            (error) => {
+//              _this.setState({
+//                isLoaded: true,
+//                //error
+//              });
+//            }
+//          )
+//      },2000);
     return (
         <div data-env={process.env.NODE_ENV} className="App" style={{ backgroundImage:`url(${bluebg})` }}>
         <Helmet>
